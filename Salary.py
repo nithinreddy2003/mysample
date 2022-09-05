a7eba7cc2eb7c2bc13c893b94209c43ed2c968b3
Nithin  Reddy Cheerapureddy <106610098+nithinreddy2003@users.noreply.github.com>
2022-09-05 11:05:54 +0530

    Update Salary.py

--- Salary.py
+++ Salary.py
@@ -5,5 +5,6 @@ model=pickle.load(pickle_in)
 
 Experience=st.number_input('Enter Years Of Experience')
 if st.button("PREDICT"):
-  salary=model.predict([[Experience]])
+  salary=model.predict([[Experience]]).squeeze()
+  salary=int(salary)
   st.success(f'SALARY PREDICTED IS {salary}')
